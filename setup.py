c1f5e7e11657aa8f127adcb4c087591634cb30bb
yonatan medan <yonatan.medan@gmail.com>
2019-04-08 14:31:06 +0300

    pip packaging

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
      name='fastindex',  
-     version='0.0.1',
+     version='0.0.2',
      author="Yonatan Medan",
      author_email="yonatan.medan@gmail.com",
      description="fast pandas selction and indexing",
@@ -16,5 +16,8 @@ setuptools.setup(
          "License :: OSI Approved :: MIT License",
          "Operating System :: OS Independent",
      ],
+     install_requires=[
+         'pandas'
+      ],
 
  )
